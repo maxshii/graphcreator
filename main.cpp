cea0a97cef9331b3eeb6616349743b92a35431a0
Max Shi <shimax@gmail.com>
2022-06-07 19:12:00 +0000

    cytkufgyuguilg

--- main.cpp
+++ main.cpp
@@ -4,7 +4,22 @@
  * 6/1/2022
  */
 
+#include <iostream>
+
+struct Vertex
+{
+  char label;
+  Vertex* next;
+};
+
 int main()
 {
-
+  Vertex* VertexList[26];
+  for(int i = 0; i < 26; i++)
+    {
+      Vertex* v = new Vertex;
+      v->label = '\0';
+      v->next = nullptr;
+      VertexList[i] = v;
+    }
 }
